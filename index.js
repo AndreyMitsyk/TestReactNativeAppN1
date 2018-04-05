9a3df9be9980f7aab217aab632db1e9d23dbef3c
Andrey Mitsyk (Akvelon) <v-anmits@microsoft.com>
2018-04-05 16:16:12 +0300

    Changed codepush configuration.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 import codePush from "react-native-code-push";
 
-var codePushOptions = { checkFrequency: codePush.CheckFrequency.MANUAL };
+var codePushOptions = { updateDialog: true, installMode: codePush.InstallMode.IMMEDIATE };
 var root = codePush(codePushOptions)(App);
 
 AppRegistry.registerComponent('NewTestApp', () => App);
